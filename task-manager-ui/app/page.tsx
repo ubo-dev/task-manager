83ab8b69d5702e25fc3b2504ed8436676eb73104
Umut Bilal Okur <umutokur11@outlook.com>
2023-10-03 09:56:24 +0300

    team and member toggle done

--- task-manager-ui/app/page.tsx
+++ task-manager-ui/app/page.tsx
@@ -1,43 +1,55 @@
 "use client";
 
 import { useState } from "react";
-import Conditional from "../components/conditional";
-import TeamForm from "../components/teamform"
+import TeamForm from "@/components/teamform";
+import React from "react";
+import MemberForm from "@/components/memberForm";
 
 export default function Home() {
+  const [toggleTeam, setToggleTeam] = useState(false);
+  const [toggleMember, setToggleMember] = useState(false);
 
-  let bool : boolean | undefined | null = null;
+  function toggleTeamForm() {
+    setToggleTeam(!toggleTeam)
+  }
 
-  function toggle() {
-    bool = !bool;
-    console.log(bool)
+  function toggleMemberForm() {
+    setToggleMember(!toggleMember)
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <div>
-        <h1 className="flex flex-col items-center p-24 text-4xl">
-          {" "}
-          Welcome to Task Manager{" "}
-        </h1>
-        <h2>
-          You can create tasks, teams, members. Assign tasks and manage them.
-        </h2>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-12">
+        <div>
+          <h1 className="flex flex-col items-center p-24 text-4xl">
+            {" "}
+            Welcome to Task Manager{" "}
+          </h1>
+          <h2>
+            You can create tasks, teams, members. Assign tasks and manage them.
+          </h2>
 
-        <div className="pt-12 flex flex-row items-center">
-          <button 
-          onClick={toggle}
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            Create Team
-          </button>
-          <button className="ml-20 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            Create Member
-          </button>
-          <button className="ml-20 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            Create Task
-          </button>
+          <div className="pt-12 flex flex-row items-center">
+            <button 
+            onClick={toggleTeamForm}
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              Create Team
+            </button>
+            <button 
+            onClick={toggleMemberForm}
+            className="ml-20 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              Create Member
+            </button>
+            <button className="ml-20 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              Create Task
+            </button>
+          </div>
+
+          <TeamForm isToggled={toggleTeam}></TeamForm>
+
+          <MemberForm isToggled={toggleMember}></MemberForm>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
